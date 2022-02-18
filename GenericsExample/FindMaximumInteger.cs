99a97b08271d5a01ced21e1d47dfac8433077fa2
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-18 20:33:20 +0530

    Using Generics to find max according to data type

--- GenericsExample/FindMaximumInteger.cs
+++ GenericsExample/FindMaximumInteger.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace GenericsExample
 {
-    internal class FindMaximumString
+    public class Maximum<E> where E:IComparable
     {
-        public static void GetMax<T>(T firstValue, T secondValue, T thirdValue) where T : System.IComparable<T>
+        public static T GetMax<T>(T firstValue, T secondValue, T thirdValue) where T : System.IComparable<T>
         {
             //// used compareto method
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
                 Console.WriteLine(firstValue + ", Is the max value.");
+            return firstValue;
             if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
                 Console.WriteLine(secondValue + ", Is the max value.");
+            return secondValue;
             if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
                 Console.WriteLine(thirdValue + ", Is the max value.");
+            return thirdValue;
+
         }
     }
 }
--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using GenericsExample;
-using System.Collections.Generic;
+﻿using GenericsExample;
 
 
-Console.WriteLine("Max value among these three string");
-FindMaximumString.GetMax("Apple", "Peach", "Banana");
-
-
-
-
+int intValue = Maximum<int>.GetMax(10, 20, 30);
+Console.WriteLine("Max value among these three integer:" + intValue);
+float floatValue = Maximum<double>.GetMax(10.2f, 35.1f, 30.5f);
+Console.WriteLine("Max value among these three floats:" + floatValue);
+string stringValue = Maximum<string>.GetMax("Apple", "Peach", "Banana");
+Console.WriteLine("Max value among these three string:" + stringValue);
