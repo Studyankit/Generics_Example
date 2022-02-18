a90bc0ceee5c91927527cde9e68da11c07f8b9dd
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-18 16:05:11 +0530

    Check maximum integer from 3 no.

---
+++ GenericsExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GenericsExample;
+using System.Collections.Generic;
+
+
+Console.WriteLine("Welcome to generincs example to find max of 3 no");
+
+FindMaximumInteger.FindIntMax(3, 2, 1);
+
+
+
+
